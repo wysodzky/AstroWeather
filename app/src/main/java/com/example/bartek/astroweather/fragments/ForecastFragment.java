d0a44cbcdcf5464ffcb46015593027a8e8f9fbb2
Bartek <bartekwysocki46@gmail.com>
2018-05-31 12:00:19 +0200

    forecast working

--- app/src/main/java/com/example/bartek/astroweather/fragments/ForecastFragment.java
+++ app/src/main/java/com/example/bartek/astroweather/fragments/ForecastFragment.java
@@ -13,6 +13,7 @@ import com.example.bartek.astroweather.Manager;
 import com.example.bartek.astroweather.R;
 import com.example.bartek.astroweather.data.Channel;
 import com.example.bartek.astroweather.data.Condition;
+import com.example.bartek.astroweather.data.Item;
 import com.example.bartek.astroweather.data.Units;
 import com.example.bartek.astroweather.service.WeatherServiceCallback;
 import com.example.bartek.astroweather.service.YahooWeatherService;
@@ -22,11 +23,55 @@ import com.example.bartek.astroweather.service.YahooWeatherService;
  */
 
 public class ForecastFragment extends Fragment implements AstroUpdate,WeatherServiceCallback {
+
+     private String unitValue = "\u00b0";
+
+    //main weather
     private ImageView forecastIconImageView;
     private TextView temperatureHighTextView;
-    private TextView temperatureLowTextView;
     private TextView locationTextView;
     private TextView conditionTextView;
+
+    //1st day
+
+    private ImageView image_1;
+    private TextView dayLabel_1;
+    private TextView hTemp_1;
+    private TextView lTemp_1;
+
+
+
+    //2 day
+
+    private ImageView image_2;
+    private TextView dayLabel_2;
+    private TextView hTemp_2;
+    private TextView lTemp_2;
+
+    //3 day
+
+    private ImageView image_3;
+    private TextView dayLabel_3;
+    private TextView hTemp_3;
+    private TextView lTemp_3;
+
+
+
+    //4 day
+
+    private ImageView image_4;
+    private TextView dayLabel_4;
+    private TextView hTemp_4;
+    private TextView lTemp_4;
+
+
+    //5 day
+
+    private ImageView image_5;
+    private TextView dayLabel_5;
+    private TextView hTemp_5;
+    private TextView lTemp_5;
+
 
     Manager manager;
 
@@ -34,15 +79,50 @@ public class ForecastFragment extends Fragment implements AstroUpdate,WeatherSer
 
     private YahooWeatherService service;
 
+
+    void initValues(View view){
+        forecastIconImageView = (ImageView)view.findViewById(R.id.forecastIconImageView);
+        temperatureHighTextView = (TextView)view.findViewById(R.id.temperatureHighTextView);
+        locationTextView = (TextView)view.findViewById(R.id.locationTextView);
+        conditionTextView = (TextView)view.findViewById(R.id.conditionTextView);
+
+
+        image_1 = (ImageView)view.findViewById(R.id.image_1);
+        image_2 = (ImageView)view.findViewById(R.id.image_2);
+        image_3 = (ImageView)view.findViewById(R.id.image_3);
+        image_4 = (ImageView)view.findViewById(R.id.image_4);
+        image_5 = (ImageView)view.findViewById(R.id.image_5);
+
+
+        dayLabel_1 = (TextView)view.findViewById(R.id.dayLabel_1);
+        dayLabel_2 = (TextView)view.findViewById(R.id.dayLabel_2);
+        dayLabel_3 = (TextView)view.findViewById(R.id.dayLabel_3);
+        dayLabel_4 = (TextView)view.findViewById(R.id.dayLabel_4);
+        dayLabel_5 = (TextView)view.findViewById(R.id.dayLabel_5);
+
+
+        hTemp_1 = (TextView)view.findViewById(R.id.hTemp_1);
+        hTemp_2 = (TextView)view.findViewById(R.id.hTemp_2);
+        hTemp_3 = (TextView)view.findViewById(R.id.hTemp_3);
+        hTemp_4 = (TextView)view.findViewById(R.id.hTemp_4);
+        hTemp_5 = (TextView)view.findViewById(R.id.hTemp_5);
+
+
+
+        lTemp_1 = (TextView)view.findViewById(R.id.lTemp_1);
+        lTemp_2 = (TextView)view.findViewById(R.id.lTemp_2);
+        lTemp_3 = (TextView)view.findViewById(R.id.lTemp_3);
+        lTemp_4 = (TextView)view.findViewById(R.id.lTemp_4);
+        lTemp_5 = (TextView)view.findViewById(R.id.lTemp_5);
+
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.forecast_fragment,container,false);
+        initValues(view);
 
-        forecastIconImageView = (ImageView)view.findViewById(R.id.forecastIconImageView);
-        temperatureHighTextView = (TextView)view.findViewById(R.id.temperatureHighTextView);
-        temperatureLowTextView = (TextView)view.findViewById(R.id.temperatureLowTextView);
-        locationTextView = (TextView)view.findViewById(R.id.locationTextView);
-        conditionTextView = (TextView)view.findViewById(R.id.conditionTextView);
         service = new YahooWeatherService(this);
         manager = Manager.getInstance();
         manager.registerForUpdates(this);
@@ -63,10 +143,48 @@ public class ForecastFragment extends Fragment implements AstroUpdate,WeatherSer
         if(isAdded()) {
             Condition[] forecast = channel.getItem().getForecast();
             Units units = channel.getUnits();
-
-            int weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[0].getCode(), null, getActivity().getPackageName());
+            Item item = channel.getItem();
+            int weatherIconId = getResources().getIdentifier("drawable/icon_" + item.getCondition().getCode(), null, getActivity().getPackageName());
             forecastIconImageView.setImageResource(weatherIconId);
-            temperatureHighTextView.setText(forecast[1].getHighTemperature() + "\u00b0" + units.getTemperature());
+            temperatureHighTextView.setText(item.getCondition().getTemperature() + unitValue + units.getTemperature());
+            conditionTextView.setText(item.getCondition().getDescription());
+            locationTextView.setText(service.getLocation());
+
+
+
+            weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[1].getCode(), null, getActivity().getPackageName());
+            image_1.setImageResource(weatherIconId);
+            hTemp_1.setText(forecast[1].getHighTemperature() + unitValue + units.getTemperature());
+            lTemp_1.setText(forecast[1].getLowTemperature() + unitValue + units.getTemperature());
+            dayLabel_1.setText(forecast[1].getDay());
+
+
+            weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[2].getCode(), null, getActivity().getPackageName());
+            image_2.setImageResource(weatherIconId);
+            hTemp_2.setText(forecast[2].getHighTemperature() + unitValue + units.getTemperature());
+            lTemp_2.setText(forecast[2].getLowTemperature() + unitValue + units.getTemperature());
+            dayLabel_2.setText(forecast[2].getDay());
+
+            weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[3].getCode(), null, getActivity().getPackageName());
+            image_3.setImageResource(weatherIconId);
+            hTemp_3.setText(forecast[3].getHighTemperature() + unitValue + units.getTemperature());
+            lTemp_3.setText(forecast[3].getLowTemperature() + unitValue + units.getTemperature());
+            dayLabel_3.setText(forecast[3].getDay());
+
+
+            weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[4].getCode(), null, getActivity().getPackageName());
+            image_4.setImageResource(weatherIconId);
+            hTemp_4.setText(forecast[4].getHighTemperature() + unitValue + units.getTemperature());
+            lTemp_4.setText(forecast[4].getLowTemperature() + unitValue + units.getTemperature());
+            dayLabel_4.setText(forecast[4].getDay());
+
+
+            weatherIconId = getResources().getIdentifier("drawable/icon_" + forecast[5].getCode(), null, getActivity().getPackageName());
+            image_5.setImageResource(weatherIconId);
+            hTemp_5.setText(forecast[5].getHighTemperature() + unitValue + units.getTemperature());
+            lTemp_5.setText(forecast[5].getLowTemperature() + unitValue + units.getTemperature());
+            dayLabel_5.setText(forecast[5].getDay());
+
         }
     }
 
