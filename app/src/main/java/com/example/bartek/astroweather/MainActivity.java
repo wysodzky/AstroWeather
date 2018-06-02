0bbba50f7a74b3dd8ec83dbc2da1a0fdd9f0f337
Bartek <bartekwysocki46@gmail.com>
2018-06-02 18:01:50 +0200

    internet connectivity needs to be done

--- app/src/main/java/com/example/bartek/astroweather/MainActivity.java
+++ app/src/main/java/com/example/bartek/astroweather/MainActivity.java
@@ -95,4 +95,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
 }
--- app/src/main/java/com/example/bartek/astroweather/service/YahooWeatherService.java
+++ app/src/main/java/com/example/bartek/astroweather/service/YahooWeatherService.java
@@ -1,19 +1,27 @@
 package com.example.bartek.astroweather.service;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
 
 import com.example.bartek.astroweather.data.Channel;
+import com.example.bartek.astroweather.fragments.WeatherFragment;
+
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
 import java.net.URL;
 import java.net.URLConnection;
+
 
 /**
  * Created by Bartek on 2018-05-21.
@@ -22,15 +30,18 @@ import java.net.URLConnection;
 public class YahooWeatherService {
 
     private WeatherServiceCallback callback;
-    private static String location="Lodz";
+    private static String location = "Lodz";
+
+
 
     private Exception error;
     private static String temperatureUnit = "C";
 
+
+
     public YahooWeatherService(WeatherServiceCallback callback) {
         this.callback = callback;
     }
-
 
 
     public static String getTemperatureUnit() {
@@ -46,11 +57,11 @@ public class YahooWeatherService {
         return location;
     }
 
-    public static void setLocation(String l){
+    public static void setLocation(String l) {
         location = l;
     }
 
-    public void refreshWeather(final String l){
+    public void refreshWeather(final String l) {
         this.location = l;
         new AsyncTask<String, Void, String>() {
             @Override
@@ -60,12 +71,17 @@ public class YahooWeatherService {
                 String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json", Uri.encode(YQL));
                 try {
                     URL url = new URL(endpoint);
+                    InputStream inputStream = null;
                     URLConnection connection = url.openConnection();
-                    InputStream inputStream = connection.getInputStream();
+
+                    inputStream = connection.getInputStream();
+
+
                     BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
                     StringBuilder result = new StringBuilder();
                     String line;
-                    while((line = reader.readLine()) != null){
+                    while ((line = reader.readLine()) != null) {
                         result.append(line);
                     }
                     return result.toString();
@@ -75,19 +91,20 @@ public class YahooWeatherService {
                 }
                 return null;
             }
-            @Override
-            protected void onPostExecute(String s){
 
-                if(s==null && error != null){
+            @Override
+            protected void onPostExecute(String s) {
+
+                if (s == null && error != null) {
                     callback.serviceFailure(error);
-                    return ;
+                    return;
                 }
                 try {
                     JSONObject data = new JSONObject(s);
                     JSONObject queryResults = data.optJSONObject("query");
                     int count = queryResults.optInt("count");
-                    if(count == 0){
-                        callback.serviceFailure(new LocationWeatherException("No weather information found"+location));
+                    if (count == 0) {
+                        callback.serviceFailure(new LocationWeatherException("No weather information found" + location));
                         return;
                     }
                     Channel channel = new Channel();
@@ -99,10 +116,12 @@ public class YahooWeatherService {
             }
         }.execute(location);
     }
-    public class LocationWeatherException extends Exception{
+
+    public class LocationWeatherException extends Exception {
 
         public LocationWeatherException(String detailMessage) {
             super(detailMessage);
         }
     }
+
 }
